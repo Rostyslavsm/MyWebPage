f2d995f7ed7b76d6c3da5281f859338352135ff5
Rostyslavsm <rmuretov@myseneca.ca>
2025-04-11 23:16:58 -0400

    bug fixes

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 import Lottie from "lottie-react";
 import animationData from "@/assets/Animation.json";
 import { useEffect, useState } from "react";
-import FloatingNeonLight from "@/components/ui/FloatingNeonLight";
 
 export default function Hero() {
   const [isVisible, setIsVisible] = useState(false);
@@ -18,7 +17,6 @@ export default function Hero() {
 
   return (
     <section className="pt-32 pb-20 overflow-hidden"> {/* Removed bg-[#0a0a0a] */}
-      <FloatingNeonLight initiallyVisible={isVisible} />
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row gap-10 items-center">
           <div 
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import FloatingNeonLight from './ui/FloatingNeonLight';
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,8 +10,9 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
+      <FloatingNeonLight />
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow z-10">
         {children}
       </main>
       <Footer />
